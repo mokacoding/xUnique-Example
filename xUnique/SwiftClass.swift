b2c5a6a51a5aef8c238cd6e8ad348a35b9f431f7
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-05-09 09:47:46 +1000

    Add a Swift class
    This is to show that xUnique works with both Objective-C and Swift projects, as the project.pbxproj treats them the same way.

---
+++ xUnique/SwiftClass.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftClass.swift
+//  xUnique
+//
+//  Created by Giovanni on 9/05/2015.
+//  Copyright (c) 2015 mokacoding. All rights reserved.
+//
+
+import Foundation
+
+public class SwiftClass {
+    public func sayHello() -> String {
+        return "Hello"
+    }
+}
