3dbb3be12ae7adcd9c20b11b9931d9d62437255b
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-05-09 10:51:53 +1000

    Add another class - Take a look at the diff

---
+++ xUnique/AnotheViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnotheViewController.h
+//  xUnique
+//
+//  Created by Giovanni on 9/05/2015.
+//  Copyright (c) 2015 mokacoding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnotheViewController : UIViewController
+
+@end
---
+++ xUnique/AnotheViewController.m
@@ -0,0 +1,13 @@
+//
+//  AnotheViewController.m
+//  xUnique
+//
+//  Created by Giovanni on 9/05/2015.
+//  Copyright (c) 2015 mokacoding. All rights reserved.
+//
+
+#import "AnotheViewController.h"
+
+@implementation AnotheViewController
+
+@end
